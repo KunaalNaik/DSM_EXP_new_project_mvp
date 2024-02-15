89edb0d16b12733b247b057cb83d185881bc4712
ankit <a.baliyan008@gmail.com>
2024-02-15 13:35:42 +0530

    Update settings.py

--- bimamitra/settings.py
+++ bimamitra/settings.py
@@ -139,7 +139,8 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 
-STATICFILES_STORAGE = 'Whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
